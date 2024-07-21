e04d8564269053a6f0e004aa9ed72d026d10289f
kiritoxkiriko <wangxinmeng1997@hotmail.com>
2024-07-22 05:58:07 +0800

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -2,6 +2,7 @@
 #git clone https://github.com/Xiaomi-Diting-Development/android_kernel_xiaomi_diting --depth=1 /tmp/android_kernel_xiaomi_diting
 #cp -r /tmp/android_kernel_xiaomi_diting/drivers/misc/hwid drivers/misc
 set -x
+apt install update && apt install -y libelf-dev
 wget https://github.com/kiritoxkiriko/build-android-kernel/raw/main/av_permissions.h -O /tmp/av_permissions.h
 wget https://github.com/kiritoxkiriko/build-android-kernel/raw/main/flask.h -O /tmp/flask.h
 cp /tmp/flask.h /tmp/av_permissions.h security/selinux/include
